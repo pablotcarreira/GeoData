5135502ece7f06696e2d050ff7c20a5fd870d56f
Pablo Carreira <pablotcarreira@gmail.com>
2019-09-25 09:41:12 -0300

    versao

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='geodata',
-      version='0.0.3',
+      version='0.0.4',
       description='OGR/GDAL High level abstraction.',
       license='BSD',
       packages=['geodata', 'geodata.tests'],
       package_data={'geodata.tests': ['data/*', 'out/*']},
       # install_requires=['pytest', 'boto3', 'peewee'],
-      python_requires='>=3.5',
+      python_requires='>=3.6',
       # setup_requires=['pytest-runner'],
       tests_require=['pytest'],
       zip_safe=False)
